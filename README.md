b43bd5b41c33854de7973dcf604ea73eba74d486
tanhaican <haican.tan@tonoinfo.com>
2021-03-26 19:31:51 +0800

    Update Readme

--- README.md
+++ README.md
@@ -1,24 +1,63 @@
 # editable-cell
 
-## Project setup
+## Install
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
+npm install editable-cell --save
 ```
 
-### Compiles and minifies for production
+### Usage
+```
+import EditableCell from 'editable-cell';
+```
+
+#### Demo
+```
+<el-table :data="tableData">
+  <el-table-column label="ID" prop="id">
+    <editable-cell slot-scope="{ row }" :show-input="row.editable" v-model="row.id" placeholder="ID">
+      <span slot="content">{{ row.id }}</span>
+    </editable-cell>
+  </el-table-column>
+  <el-table-column label="Name" prop="name">
+    <editable-cell slot-scope="{ row }" :show-input="row.editable" v-model="row.name" placeholder="Name">
+      <span slot="content">{{ row.name }}</span>
+    </editable-cell>
+  </el-table-column>
+  <el-table-column label="Sex" prop="sex">
+    <editable-cell
+      slot-scope="{ row }"
+      :show-input="row.editable"
+      v-model="row.sex"
+      true-label="M"
+      false-label="F"
+      editable-component="el-checkbox" placeholder="Sex"
+    >
+      <span slot="content">{{ row.sex }}</span>
+    </editable-cell>
+  </el-table-column>
+  <el-table-column label="Age" prop="age">
+    <editable-cell slot-scope="{ row }" :show-input="row.editable" v-model="row.age" placeholder="Age">
+      <span slot="content">{{ row.age }}</span>
+    </editable-cell>
+  </el-table-column>
+</el-table>
+
+
+tableData: [
+  {editable: false, id: 1, name: 'Danny', sex: 'M', age: 18},
+  {editable: false, id: 2, name: 'Tony', sex: 'M', age: 25},
+  {editable: false, id: 3, name: 'Emily', sex: 'F', age: 21},
+  {editable: false, id: 4, name: 'Green', sex: 'M', age: 26}
+]
+```
+
+### Configs
 ```
 npm run build
 ```
 
-### Lints and fixes files
+### Run for demo
 ```
-npm run lint
+npm run serve
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Visit : http://localhost:8080
---
+++ src/Demo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <h1>Table with editable cells</h1>
+
+    <el-table :data="tableData">
+      <el-table-column label="ID" prop="id">
+        <editable-cell slot-scope="{ row }" :show-input="row.editable" v-model="row.id" placeholder="ID">
+          <span slot="content">{{ row.id }}</span>
+        </editable-cell>
+      </el-table-column>
+      <el-table-column label="Name" prop="name">
+        <editable-cell slot-scope="{ row }" :show-input="row.editable" v-model="row.name" placeholder="Name">
+          <span slot="content">{{ row.name }}</span>
+        </editable-cell>
+      </el-table-column>
+      <el-table-column label="Sex" prop="sex">
+        <editable-cell
+          slot-scope="{ row }"
+          :show-input="row.editable"
+          v-model="row.sex"
+          true-label="M"
+          false-label="F"
+          editable-component="el-checkbox" placeholder="Sex"
+        >
+          <span slot="content">{{ row.sex }}</span>
+        </editable-cell>
+      </el-table-column>
+      <el-table-column label="Age" prop="age">
+        <editable-cell slot-scope="{ row }" :show-input="row.editable" v-model="row.age" placeholder="Age">
+          <span slot="content">{{ row.age }}</span>
+        </editable-cell>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import EditableCell from 'editable-cell';
+
+export default {
+  name: 'App',
+  components: {
+    EditableCell
+  },
+  data() {
+    return {
+      tableData: [
+        {editable: false, id: 1, name: 'Danny', sex: 'M', age: 18},
+        {editable: false, id: 2, name: 'Tony', sex: 'M', age: 25},
+        {editable: false, id: 3, name: 'Emily', sex: 'F', age: 21},
+        {editable: false, id: 4, name: 'Green', sex: 'M', age: 26}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
